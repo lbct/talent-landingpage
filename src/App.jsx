79521e3b91543165192d5fd10cb087dd721f460c
Bernardo <lbct95@gmail.com>
2025-10-02 12:02:13 -0400

    Icons & title

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,11 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import Header from './components/header/header'
 import Footer from './components/footer/footer'
 import LandingBody from './components/landingbody/landingbody'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Card from './components/custommers/Card'
 import { CostumersSection } from './components/custommers/CostumersSection'
 import { IntroSection } from './components/custommers/IntroSection'
-import MetricCard from './components/custommers/MetricCard'
 import Prueba from './components/vistaprueba/prueba' // Import con mayúscula
 
 function App() {
